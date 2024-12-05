6c4d7428c303114ced703a84066e2fc0414c6277
Artem Zamotaev <artemmori2905@gmail.com>
2024-12-05 22:31:25 +0200

    thrid commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=15)
+    content = models.TextChoices()
+    published_date = models.DateTimeField()
+    photo = models.ImageField(upload_to='C:\Python\my_blog\MyBlog\photoes', height_field=None, width_field=None, max_length=100)
