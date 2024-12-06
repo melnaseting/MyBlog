3aa72d30cd3f9ed396bafc7cbb1df8d8a55eb4b6
Artem Zamotaev <artemmori2905@gmail.com>
2024-12-06 21:20:32 +0200

    sixth commit

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("posts/",include("blog.urls")),
 ]
+urlpatterns += static(MEDIA_URL,document_root=MEDIA_ROOT)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,27 @@
 from django.db import models
-
+from django.utils.timezone import now
+from datetime import timedelta
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=15)
+    bio = models.TextField(null=True, blank=True)
+    def __str__(self) -> str:
+        return self.name
 
 class Post(models.Model):
+    def published_recently(self):
+        return now()- self.published_date <= timedelta(days=7)
     title = models.CharField(max_length=15)
     content = models.TextField(blank=True , null=False)
     published_date = models.DateTimeField()
-    photo = models.ImageField(upload_to='static/', height_field=None, width_field=None, max_length=100)
+    photo = models.ImageField(upload_to='media/', height_field=None, width_field=None, max_length=100)
+    author = models.ForeignKey(Author , on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self) -> str:
+        return f"{self.title} , {self.published_date}"
+    
+
+
+
+
     
