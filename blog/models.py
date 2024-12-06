bd95e6b9a697bf358c9d3b9a5028f6b80ccfb291
Artem Zamotaev <artemmori2905@gmail.com>
2024-12-06 19:04:50 +0200

    fifth commit

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,5 @@ class Post(models.Model):
     title = models.CharField(max_length=15)
     content = models.TextField(blank=True , null=False)
     published_date = models.DateTimeField()
-    photo = models.ImageField(upload_to='static', height_field=None, width_field=None, max_length=100)
+    photo = models.ImageField(upload_to='static/', height_field=None, width_field=None, max_length=100)
     
